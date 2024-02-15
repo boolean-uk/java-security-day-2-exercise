54862d1d125d048fe5ebee89a7e964112f9c957c
msg <mats.gravem@no.experis.com>
2024-02-15 11:12:38 +0100

    Implement authentication from workshop

---
+++ src/main/java/com/booleanuk/api/enums/ERole.java
@@ -0,0 +1,7 @@
+package com.booleanuk.api.enums;
+
+public enum ERole {
+    ROLE_USER,
+    ROLE_MODERATOR,
+    ROLE_ADMIN
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO
+    roles(name)
+VALUES
+    ('ROLE_USER'),
+    ('ROLE_MODERATOR'),
+    ('ROLE_ADMIN');
