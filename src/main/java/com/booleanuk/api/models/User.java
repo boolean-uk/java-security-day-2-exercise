42e0c33b2063a9af6dad6fc2615f0881571a1af8
Alexander Gatland <alexander.gatland@no.experis.com>
2024-02-15 14:50:24 +0100

    Add model classes for item and borrowing

--- src/main/java/com/booleanuk/api/models/User.java
+++ src/main/java/com/booleanuk/api/models/User.java
@@ -1,5 +1,6 @@
 package com.booleanuk.api.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -8,6 +9,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @NoArgsConstructor
@@ -41,6 +43,10 @@ public class User {
         joinColumns = @JoinColumn(name = "user_id"),
         inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles = new HashSet<>();
+
+    @OneToMany(mappedBy = "user")
+    @JsonIgnore
+    private List<BorrowedItem> borrowedItem;
 
     public User(String username, String email, String password) {
         this.username = username;
