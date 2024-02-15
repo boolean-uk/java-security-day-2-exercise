2759414e13f4b633fd4b64d9d2a8619b928771d1
Enock97 <enockladu@hotmail.com>
2024-02-15 18:00:24 +0100

    Completed core exercise.

---
+++ src/main/java/com/booleanuk/api/library/repositories/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.booleanuk.api.library.repositories;
+
+import com.booleanuk.api.library.models.ERole;
+import com.booleanuk.api.library.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+
+    Optional<Role> findByName(ERole name);
+}
