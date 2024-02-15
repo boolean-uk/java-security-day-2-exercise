d8adf239738cae7ec07be9eea8a47d50bf984c1c
Alexander Gatland <alexander.gatland@no.experis.com>
2024-02-15 16:33:02 +0100

    Loan code from day 1 and create the get user loaned, history and game history endpoints

--- src/main/java/com/booleanuk/api/controllers/BorrowedItemController.java
+++ src/main/java/com/booleanuk/api/controllers/BorrowedItemController.java
@@ -1,8 +1,11 @@
 package com.booleanuk.api.controllers;
 
+import com.booleanuk.api.dto.BorrowedItemDto;
 import com.booleanuk.api.models.BorrowedItem;
-import com.booleanuk.api.models.Item;
 import com.booleanuk.api.models.User;
+import com.booleanuk.api.payload.response.BorrowedItemListResponse;
+import com.booleanuk.api.payload.response.ErrorResponse;
+import com.booleanuk.api.payload.response.Response;
 import com.booleanuk.api.repository.BorrowedItemRepository;
 import com.booleanuk.api.repository.ItemRepository;
 import com.booleanuk.api.repository.UserRepository;
@@ -25,50 +28,32 @@ public class BorrowedItemController {
 
     @Autowired
     ItemRepository itemRepository;
-/*
+
+    //@GetMapping
+    //public ResponseEntity<Response<?>> getAllCurrent()
+
     @GetMapping("/users/{userId}")
-    public ResponseEntity<?> getAllForUser(@PathVariable int userId) {
-        return ResponseEntity.ok(new Response<>(this.repository.findBorrowedGamesByUserId(userId)));
+    public ResponseEntity<Response<?>> getAllCurrentForUser(@PathVariable int userId) {
+        BorrowedItemListResponse borrowedItemListResponse = new BorrowedItemListResponse();
+        borrowedItemListResponse.set(this.repository.findCurrentlyBorrowedItemsByUserId(userId));
+        return ResponseEntity.ok(borrowedItemListResponse);
     }
 
-    @GetMapping("/games/{gameId}")
-    public ResponseEntity<Response<List<BorrowedGameDto>>> getAllForGame(@PathVariable int gameId) {
-        return ResponseEntity.ok(new Response<>(this.repository.findBorrowedGamesByGameId(gameId)));
+    //@GetMapping("/history")
+
+
+    @GetMapping("/history/users/{userId}")
+    public ResponseEntity<Response<?>> getAllHistoryForUser(@PathVariable int userId) {
+        BorrowedItemListResponse borrowedItemListResponse = new BorrowedItemListResponse();
+        borrowedItemListResponse.set(this.repository.findPreviouslyBorrowedItemsByUserId(userId));
+        return ResponseEntity.ok(borrowedItemListResponse);
     }
 
-    @PostMapping("/users/{userId}/games/{gameId}")
-    public ResponseEntity<Response<BorrowedGameDto>> lendGame(@PathVariable int userId, @PathVariable int gameId) {
-        // Check if game is currently lent out
-        if (!this.repository.findIsBorrowedByGameId(gameId).isEmpty()) {
-            throw new ApiException(HttpStatus.INTERNAL_SERVER_ERROR, "Game is already lent out");
-        }
-        User user = this.userRepository.findById(userId)
-                .orElseThrow(() -> new ApiException(HttpStatus.NOT_FOUND, "user not found"));
-        Game game = this.gameRepository.findById(gameId)
-                .orElseThrow(() -> new ApiException(HttpStatus.NOT_FOUND, "game not found"));
-        if (user.getDob().isAfter(LocalDate.now().minusYears(game.getAgeRating()))) {
-            throw new ApiException(HttpStatus.BAD_REQUEST, "User is too young!!!");
-        }
-        BorrowedGame createdBorrowedGame = this.repository.save(new BorrowedGame(user, game));
-
-        return new ResponseEntity<>(new Response<>(this.translateToDto(createdBorrowedGame)), HttpStatus.CREATED);
+    @GetMapping("/history/items/{itemId}")
+    public ResponseEntity<Response<?>> getAllForItem(@PathVariable int itemId) {
+        BorrowedItemListResponse borrowedItemListResponse = new BorrowedItemListResponse();
+        borrowedItemListResponse.set(this.repository.findBorrowedItemsByItemId(itemId));
+        return ResponseEntity.ok(borrowedItemListResponse);
     }
 
-    @PutMapping("/users/{userId}/games/{gameId}")
-    public ResponseEntity<Response<BorrowedGameDto>> returnGame(@PathVariable int userId, @PathVariable int gameId) {
-        List<BorrowedGame> borrowedGames = this.repository.findIsBorrowedByGameId(gameId);
-        if (borrowedGames.isEmpty()) {
-            throw new ApiException(HttpStatus.NOT_FOUND, "game not found");
-        } else if (borrowedGames.get(0).getUser().getId() != userId) {
-            throw new ApiException(HttpStatus.NOT_FOUND, "Game not lent to this user");
-        }
-        BorrowedGame borrowedGame = borrowedGames.get(0);
-        borrowedGame.setReturnDate(LocalDate.now());
-
-        return new ResponseEntity<>(new Response<>(this.translateToDto(this.repository.save(borrowedGame))), HttpStatus.CREATED);
-    }
-
-    public BorrowedGameDto translateToDto(BorrowedGame borrowedGame) {
-        return new BorrowedGameDto(borrowedGame.getId(), borrowedGame.getGame().getTitle(), borrowedGame.getUser().getUsername(), borrowedGame.getBorrowDate(), borrowedGame.getReturnDate());
-    }*/
 }
--- src/main/java/com/booleanuk/api/payload/response/BorrowedItemResponse.java
+++ src/main/java/com/booleanuk/api/payload/response/BorrowedItemResponse.java
@@ -1,6 +1,7 @@
 package com.booleanuk.api.payload.response;
 
+import com.booleanuk.api.dto.BorrowedItemDto;
 import com.booleanuk.api.models.BorrowedItem;
 
-public class BorrowedItemResponse extends Response<BorrowedItem> {
+public class BorrowedItemResponse extends Response<BorrowedItemDto> {
 }
